0fd3bcf82b770bef84984389da3de1ffea53915c
mdeandwibekti <mdeandwibekti@gmail.com>
2025-11-10 09:00:14 +0700

    mengset agar dapat di jalankan

---
+++ app/src/main/java/com/example/questnavigasitugas_042/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.questnavigasitugas_042
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.questnavigasitugas_042.navigasi.AppNavHost
+import com.example.questnavigasitugas_042.ui.theme.QuestNavigasiTugas_042Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            QuestNavigasiTugas_042Theme {
+                Surface (
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AppNavHost()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    QuestNavigasiTugas_042Theme {
+        Greeting("Android")
+    }
+}
