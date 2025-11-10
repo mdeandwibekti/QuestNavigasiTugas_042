5f87155e862df9d052fcec41f26ebc7e9ac7a5d8
mdeandwibekti <mdeandwibekti@gmail.com>
2025-11-10 07:45:06 +0700

    membuat fun listpeserta dan membuat scaffold topbar

---
+++ app/src/main/java/com/example/questnavigasitugas_042/view/listperserta.kt
@@ -0,0 +1,50 @@
+package com.example.questnavigasitugas_042.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.questnavigasitugas_042.R
+import com.example.questnavigasitugas_042.viewmodel.FormData
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HalamanListPeserta(
+    listpeserta: List<FormData>,
+    onKeFormulirClicked:() -> Unit,
+    onBerandaClicked: () -> Unit
+) {
+    Scaffold(
+        modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(stringResource(id = R.string.list_title), color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorResource(id = R.color.purple_700)
+                )
+            )
+        }
+    )
