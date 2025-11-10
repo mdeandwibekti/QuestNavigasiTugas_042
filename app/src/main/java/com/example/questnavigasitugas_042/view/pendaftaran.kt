112c231e6f4935023e973d7c624bc1a05801f0de
mdeandwibekti <mdeandwibekti@gmail.com>
2025-11-10 08:07:41 +0700

    membuat fun formulir dengan menentukan beberapa list sepetri jenisK dan agama dan juga membuat var dan val

---
+++ app/src/main/java/com/example/questnavigasitugas_042/view/pendaftaran.kt
@@ -0,0 +1,58 @@
+package com.example.questnavigasitugas_042.view
+
+import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.questnavigasitugas_042.R
+import com.example.questnavigasitugas_042.viewmodel.DataViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HalamanFormulir(
+    jenisK: List<String> = listOf("Laki-laki", "Perempuan"),
+    listAgama: List<String> = listOf("Islam", "Kristen", "Katolik", "Hindu", "Budha", "Konghucu"),
+    viewModel: DataViewModel,
+    onNavigateBack: () -> Unit
+) {
+    var nama by remember { mutableStateOf("") }
+    var jenisKelamin by remember { mutableStateOf(jenisK[0]) }
+    var pemeran by remember { mutableStateOf("") }
+    var agama by remember { mutableStateOf(listAgama[0]) }
+    var isAgamaDropdownExpanded by remember { mutableStateOf(false) }
+
+    val context = LocalContext.current
+    val toastMessage = stringResource(id = R.string.submit_toast)
+
