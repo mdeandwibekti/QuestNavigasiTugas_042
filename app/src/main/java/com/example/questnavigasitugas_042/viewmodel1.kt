25b770c60fb84268607396fc7e8b909141100e8f
mdeandwibekti <mdeandwibekti@gmail.com>
2025-11-10 08:49:41 +0700

    membuat init val datalist yang sudah ada

--- app/src/main/java/com/example/questnavigasitugas_042/viewmodel1.kt
+++ app/src/main/java/com/example/questnavigasitugas_042/viewmodel1.kt
@@ -19,5 +19,39 @@ class DataViewModel : ViewModel() {
         val pesertaBaru = FormData(nama, jk, pemeran, agama)
         _pesertaList.add(pesertaBaru)
     }
-
+    init {
+        val dataList = listOf(
+            FormData(
+                nama = "Arbani Yazid",
+                gender = "Laki - Laki",
+                pemeran = "Dilan",
+                agama = "Islam"
+            ),
+            FormData(
+                nama = "Zee Ashadel",
+                gender = "Perempuan",
+                pemeran = "Anchika",
+                agama = "Islam"
+            ),
+            FormData(
+                nama = "Caitlin Halderman",
+                gender = "Perempuan",
+                pemeran = "Milea",
+                agama = "Islam"
+            ),
+            FormData(
+                nama = "Jefan Nathanio",
+                gender = "Laki - Laki",
+                pemeran = "Bono",
+                agama = "Kristen"
+            ),
+            FormData(
+                nama = "Daffa Wardhana",
+                gender = "Laki - Laki",
+                pemeran = "Yadit",
+                agama = "Islam"
+            )
+        )
+        _pesertaList.addAll(dataList)
+    }
 }
